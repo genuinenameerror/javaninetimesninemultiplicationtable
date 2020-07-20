4837c39671179f7646254a3fd7c71c4a2f8f342f
genuinenameerror <genuinenameerror@users.noreply.github.com>
2020-07-20 16:01:13 +0900

    구구단 연습 시작

---
+++ src/ninetimesninemultiplicationtable/ninetimesninemultiplicationtable.java
@@ -0,0 +1,60 @@
+package ninetimesninemultiplicationtable;
+
+public class ninetimesninemultiplicationtable {
+	public static void main(String[] args) {
+		// 2단
+		System.out.println("2 x 1 = " + 2*1);
+		System.out.println("2 x 2 = " + 2*2);
+		System.out.println("2 x 3 = " + 2*3);
+		System.out.println("2 x 4 = " + 2*4);
+		System.out.println("2 x 5 = " + 2*5);
+		System.out.println("2 x 6 = " + 2*6);
+		System.out.println("2 x 7 = " + 2*7);
+		System.out.println("2 x 8 = " + 2*8);
+		System.out.println("2 x 9 = " + 2*9);
+		//3단
+		System.out.println("3 x 1 = " + 3*1);
+		System.out.println("3 x 2 = " + 3*2);
+		System.out.println("3 x 3 = " + 3*3);
+		System.out.println("3 x 4 = " + 3*4);
+		System.out.println("3 x 5 = " + 3*5);
+		System.out.println("3 x 6 = " + 3*6);
+		System.out.println("3 x 7 = " + 3*7);
+		System.out.println("3 x 8 = " + 3*8);
+		System.out.println("3 x 9 = " + 3*9);
+		
+		int number1 = 4;
+		int number2 = 1;
+		int result = number1 * number2;
+		System.out.println(number1 + " x " + number2 + " = "+result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		number2 = number2 + 1;
+		result = number1 * number2; 
+		System.out.println(number1 + " x " + number2 + " = " + result);
+		
+		
+		
+		
+	}
+
+}
