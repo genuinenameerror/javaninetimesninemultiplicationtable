89e9cc116a2145f6154ce98a9e33e9cf396009b0
genuinenameerror <genuinenameerror@users.noreply.github.com>
2020-07-20 20:02:03 +0900

    2-8 88단 등 다양한 테이블 출력

--- src/ninetimesninemultiplicationtable/ninetimesninemultiplicationtableMain.java
+++ src/ninetimesninemultiplicationtable/ninetimesninemultiplicationtableMain.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 public class ninetimesninemultiplicationtableMain {
 	public static void main(String[] args) {	
 		System.out.println("");
-		System.out.println("몇 단부터 출력할까요? ");
+		System.out.println("몇 단을 출력할까요? ");
 		Scanner scanner = new Scanner(System.in); 
 		int newnumber = scanner.nextInt(); // 입력된 정수값 저장 
 		System.out.println("입력된 수 : " + newnumber);
-		if (newnumber <2) {
-			System.out.println("잘못 입력했습니다");
-			
-		}
-		else if (newnumber>9) {
-			System.out.println("잘못 입력했습니다");
-		}
-		else {
-			int[] k = ninetimesninemultiplicationtable.calculate(newnumber);
-			ninetimesninemultiplicationtable.print(k);
-			System.out.println(" ");
-			System.out.println("전체 구구단");
-			for (int i=2;i<10;i++) {
+//		if (newnumber <2) {
+//			System.out.println("잘못 입력했습니다");
+//			
+//		}
+//		else if (newnumber>9) {
+//			System.out.println("잘못 입력했습니다");
+//		}
+//		else {
+//			int[] k = ninetimesninemultiplicationtable.calculate(newnumber);
+//			ninetimesninemultiplicationtable.print(k);
+//			System.out.println(" ");
+			System.out.println(newnumber + " x " + newnumber + "단 출력");
+			for (int i=2;i<newnumber+1;i++) {
 				int[] general = ninetimesninemultiplicationtable.calculate(i);
 				ninetimesninemultiplicationtable.print(general);
 			}
-		}
+//		}
 	}	
-
 }
